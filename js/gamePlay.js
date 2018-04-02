a33abc92e17c9d297a1c467c642c4d60ff1f381d
mychanak <alice.uniyhkust2015@gmail.com>
2018-04-03 02:38:19 +0800

    random generate plaform

--- js/gamePlay.js
+++ js/gamePlay.js
@@ -27,6 +27,32 @@ function collisionWithceiling(){
 function checkGameOver(){
 
 }
+
+
+function makePlatform(id) {
+    console.log("id "+id);
+    var tempId = id+"svg";
+    document.getElementById(id+"svg").setAttribute("x",  Math.floor((Math.random() * 2000)));
+    document.getElementById(id+"svg").setAttribute("y", 2500);
+    var newid="#"+id;
+     var temp =  Math.floor((Math.random() * 2000)); 
+    setTimeout(function() {
+        $(newid).css("animationPlayState", "running");
+    }, Math.floor((Math.random() * 10000)+2000));
+
+
+}
+
+function init(){
+    document.getElementById("platform2"+"svg").setAttribute("x",  Math.floor((Math.random() * 2000)));
+    document.getElementById("platform3"+"svg").setAttribute("x",  Math.floor((Math.random() * 2000)));
+    document.getElementById("platform4"+"svg").setAttribute("x",  Math.floor((Math.random() * 2000)));
+    document.getElementById("platform5"+"svg").setAttribute("x",  Math.floor((Math.random() * 2000)));
+    document.getElementById("platform2"+"svg").setAttribute("y",  Math.floor((Math.random() * 2000)+1000));
+    document.getElementById("platform3"+"svg").setAttribute("y",  Math.floor((Math.random() * 2000)+500));
+
+
+}
 $(document).ready(function(){
     var r = 0;
     var key ={
@@ -49,4 +75,53 @@ $(document).ready(function(){
                 break;
         }
     });
+
+    $("#platform1").on("animationend", function() {
+            // You need to stop the animation here
+            $("#platform1").css("display","none");
+
+        });
+    $("#platform2").on("animationiteration", function() {
+            // You need to stop the animation here
+            $("#platform2").css("animation-play-state","paused");
+            makePlatform("platform2");
+
+        });
+
+
+    $("#platform3").on("animationiteration", function() {
+            // You need to stop the animation here
+            $("#platform3").css("animation-play-state","paused");
+            makePlatform("platform3");
+
+        });
+
+     $("#platform4").on("animationiteration", function() {
+            // You need to stop the animation here
+            $("#platform4").css("animation-play-state","paused");
+            makePlatform("platform4");
+
+        });
+      $("#platform5").on("animationiteration", function() {
+            // You need to stop the animation here
+            $("#platform5").css("animation-play-state","paused");
+            makePlatform("platform5");
+
+        });
+
+    init();
+    setTimeout(function() {
+        $("#platform1").css("animationPlayState", "running");
+        $("#platform2").css("animationPlayState", "running");
+        $("#platform3").css("animationPlayState", "running");
+        makePlatform("platform4");
+        makePlatform("platform5");
+        $("#player").css("animationPlayState", "running");
+        $(".landscape").css("animationPlayState", "running");
+        $("#landscape").css("animationPlayState", "running")
+        $(".stingPlatform").css("animationPlayState", "running");
+    }, 3000);
+
+
+
 });

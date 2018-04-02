603b7d2b57d10e8c74d00d2631af678c67c4817b
robbieyang <robbieyang1996@gmail.com>
2018-04-02 23:07:57 +0800

    control method added

--- js/gamePlay.js
+++ js/gamePlay.js
@@ -1,17 +1,22 @@
-function moveLeft(){
-    
-}
-
-function moveRight(){
-
+function move(n){
+    $("#playermove").css("transform", "translateX(" + n + "px)")
 }
 function jump(){
     
 }
 
 //return true if the player's bottom <= platform's top
-function collisionWithPlatforms(height){
+function collisionWithPlatforms(nameOfObject){
+    var platform = $(nameOfObject).css("transform");
+    var platform_x = parseFloat(platform.split(" ")[4]);
+    var platform_y = parseFloat(platform.split(" ")[5]);
 
+    var player_xy = $("#player").css("transform");
+    var player_x = parseFloat(player_xy.split(" ")[4]);
+    var player_y = parseFloat(player_xy.split(" ")[5]);
+
+
+    
 }
 
 // return true if the player's top <= ceiling bottom position
@@ -29,14 +34,18 @@ $(document).ready(function(){
         RIGHT: 39,
         UP: 38
     };
+
+    console.log($(".landscape").position())
     $(document).on("keydown", function(e){
         switch(e.which){
             case key.LEFT:
-                moveLeft();
-                //console.log($("#player").position().left);
+                r -= 10;
+                move(r);
+                console.log($(".platform").css("transform"));
                 break;
             case key.RIGHT:
-                moveRight();
+                r += 10;
+                move(r);
                 break;
         }
     });
